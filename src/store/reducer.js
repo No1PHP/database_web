17c9b3602e82ad9839cc5789c75980a069822576
LAST-iMP <xiaoshibang@126.com>
2020-06-05 21:16:58 +0800

    debug

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,6 +1,5 @@
 import { combineReducers } from 'redux-immutable';
 import { reducer as headerReducer } from '../common/header/store';
-import { reducer as homeReducer } from '../pages/home/store';
 import { reducer as materialReducer } from '../pages/Material/store';
 import { reducer as loginReducer } from '../pages/login/store';
 import { reducer as operationReducer } from '../pages/OperationRecord/store';
